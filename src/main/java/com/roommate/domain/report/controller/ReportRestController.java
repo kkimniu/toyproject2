cb845a26aa99bae4196504a2a406d4a9ad54c05d
Kim Dong Min <kkimniu@gmail.com>
2026-05-17 13:46:34 +0900

    feat: add reporter resolution view

--- src/main/java/com/roommate/domain/report/controller/ReportRestController.java
+++ src/main/java/com/roommate/domain/report/controller/ReportRestController.java
@@ -1,4 +1,25 @@
 package com.roommate.domain.report.controller;
 
+import com.roommate.common.security.UserDetailsImpl;
+import com.roommate.domain.report.dto.MyReportListItemResponse;
+import com.roommate.domain.report.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reports")
+@RequiredArgsConstructor
 public class ReportRestController {
+
+    private final ReportService reportService;
+
+    @GetMapping("/me")
+    public List<MyReportListItemResponse> getMyReports(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return reportService.getMyReports(userDetails.getMemberId());
+    }
 }
--- src/main/java/com/roommate/domain/report/service/ReportSerivceImp.java
+++ src/main/java/com/roommate/domain/report/service/ReportSerivceImp.java
@@ -1,4 +1,20 @@
 package com.roommate.domain.report.service;
 
-public class ReportSerivceImp implements ReportService{
+import com.roommate.domain.report.dto.MyReportListItemResponse;
+import com.roommate.domain.report.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReportSerivceImp implements ReportService {
+
+    private final ReportRepository reportRepository;
+
+    @Override
+    public List<MyReportListItemResponse> getMyReports(Long reporterId) {
+        return reportRepository.findMyReports(reporterId);
+    }
 }
